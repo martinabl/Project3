76ba274bf0de4077bc99c55111652793635b42ce
martinabl <matinka271664@gmail.com>
2025-05-19 23:54:32 +0200

    KOnečná upravení aplikace kvůli spuštění
    kontrola a upravení aplikace, aby se otevřela a spustila

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import org.example.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,9 +9,16 @@ import org.example.service.UserService;
 
 import java.io.IOException;
 
+
 @Controller
-@RequestMapping("users")
 public class UserController {
+
+    @Autowired
+
+    @GetMapping("/")
+    public String redirectToUsers() {
+        return "redirect:/users";
+    }
 
     private final UserService userService;
 
@@ -18,19 +26,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
+    @GetMapping("/users")
     public String getAllUsers(Model model) {
         model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
 
-    @GetMapping("/new")
+    @GetMapping("/users/new")
     public String showCreateForm(Model model) {
         model.addAttribute("user", new User());
         return "create_user";
     }
 
-    @PostMapping
+    @PostMapping("/users")
     public String createUser(@ModelAttribute User user) {
         try {
             userService.createUser(user);
@@ -40,22 +48,22 @@ public class UserController {
         }
     }
 
-    @GetMapping("/edit/{id}")
+    @GetMapping("/users/edit/{id}")
     public String showEditForm(@PathVariable Long id, Model model) {
         User user = userService.getUserById(id)
                 .orElseThrow(() -> new RuntimeException("Uživatel nenalezen"));
         model.addAttribute("user", user);
-        return "edit_user";
+        return "editor_user";
     }
 
-    @PostMapping("/update/{id}")
+    @PostMapping("/users/update/{id}")
     public String updateUser(@PathVariable Long id, @ModelAttribute User user) {
         user.setId(id);
         userService.updateUser(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/delete/{id}")
+    @GetMapping("/users/delete/{id}")
     public String deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return "redirect:/users";
